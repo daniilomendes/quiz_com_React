f8448f106a713e628781a6046ba4d66c3ba84ba5
Danilo Mendes <danilo.mirandha@gmail.com>
2023-09-08 10:44:31 -0300

    Quiz com React completo

---
+++ src/components/PickCategory.jsx
@@ -0,0 +1,35 @@
+import { useContext } from "react"
+import { QuizContext } from "../context/quiz"
+
+
+import Category from "../img/category.svg"
+import "./PickCategory.css"
+
+
+
+const PickCategory = () => {
+    const [quizState, dispatch] = useContext(QuizContext)
+
+    const chooseCategoryAndReorderQuestion = (category) => {
+        dispatch({type: "START_GAME", payload: category})
+
+        dispatch({type: "REORDER_QUESTIONS"})
+    }
+
+
+
+  return (
+    <div id="category">
+        <h2>Escolha uma categoria</h2>
+        <p>As perguntas serão referentes a uma das linguagens abaixo:</p>
+        <div>
+            {quizState.questions.map((question) => (
+                <button onClick={() => chooseCategoryAndReorderQuestion(question.category)} key={question.category} >{question.category}</button>
+            ))}
+        </div>
+        <img src={Category} alt="Categorias do Quiz" />
+    </div>
+  )
+}
+
+export default PickCategory
